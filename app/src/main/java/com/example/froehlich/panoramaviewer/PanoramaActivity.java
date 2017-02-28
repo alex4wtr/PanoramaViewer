403a103e1e0c83cc1862c6eff137c3aa29f4daff
Alex F <a.froehlich@wtr-architekten.de>
2017-02-28 09:59:06 +0100

    Setting Up Android Project

---
+++ app/src/main/java/com/example/froehlich/panoramaviewer/PanoramaActivity.java
@@ -0,0 +1,13 @@
+package com.example.froehlich.panoramaviewer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PanoramaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_panorama);
+    }
+}
